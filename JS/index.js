411ddf2cf5ce72feeb4ac72ca2658668641efb59
Mikael Fernandes Moreira <mikaelfernandesmoreira@gmail.com>
2023-05-19 13:55:16 -0300

    adição do modelo python

--- JS/index.js
+++ JS/index.js
@@ -26,26 +26,34 @@ const App = new Vue ({
     },
     methods: {
         pegaMarcas: function (){
+            console.log((`https://parallelum.com.br/fipe/api/v1/${this.tipo_veiculo}/marcas`))
             $.get(`https://parallelum.com.br/fipe/api/v1/${this.tipo_veiculo}/marcas`)
             .done(function(response) {
+                console.log(response)
                 App.marcas = response
             });
         },
         pegaModelos: function (){
+            console.log(`https://parallelum.com.br/fipe/api/v1/${this.tipo_veiculo}/marcas/${this.marca_veiculo}/modelos`)
             $.get(`https://parallelum.com.br/fipe/api/v1/${this.tipo_veiculo}/marcas/${this.marca_veiculo}/modelos`)
             .done(function(response) {
+                console.log(response)
                 App.modelos = response['modelos']
             });
         },
         pegaAnos: function (){
+            console.log(`https://parallelum.com.br/fipe/api/v1/${this.tipo_veiculo}/marcas/${this.marca_veiculo}/modelos/${this.modelo_veiculo}/anos`)
             $.get(`https://parallelum.com.br/fipe/api/v1/${this.tipo_veiculo}/marcas/${this.marca_veiculo}/modelos/${this.modelo_veiculo}/anos`)
             .done(function(response) {
+                console.log(response)
                 App.anos = response
             });
         },
         pegaDados: function (){
+            console.log(`https://parallelum.com.br/fipe/api/v1/${this.tipo_veiculo}/marcas/${this.marca_veiculo}/modelos/${this.modelo_veiculo}/anos/${this.ano_veiculo}`)
             $.get(`https://parallelum.com.br/fipe/api/v1/${this.tipo_veiculo}/marcas/${this.marca_veiculo}/modelos/${this.modelo_veiculo}/anos/${this.ano_veiculo}`)
             .done(function(response) {
+                console.log(response)
                 App.dados = response
             });
         }
